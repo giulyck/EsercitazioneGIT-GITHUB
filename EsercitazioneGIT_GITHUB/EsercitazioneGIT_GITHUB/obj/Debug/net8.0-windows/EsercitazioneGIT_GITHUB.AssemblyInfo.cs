628a7cdb16b4800199498ed03a86708b99bba6c9
Giulia Cocka <GIULIA.COCKA@local.ittcesena.it>
2024-10-21 09:19:56 +0200

    Correzione Classe Biblioteca

--- EsercitazioneGIT_GITHUB/EsercitazioneGIT_GITHUB/obj/Debug/net8.0-windows/EsercitazioneGIT_GITHUB.AssemblyInfo.cs
+++ EsercitazioneGIT_GITHUB/EsercitazioneGIT_GITHUB/obj/Debug/net8.0-windows/EsercitazioneGIT_GITHUB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsercitazioneGIT_GITHUB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e577c1f965a3212f67b6fac1d6e2f7c83f217a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a42fabcdc508ccc5b814fe062da248a16ef7b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsercitazioneGIT_GITHUB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsercitazioneGIT_GITHUB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
